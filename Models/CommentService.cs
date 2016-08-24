169ee0c568b2f7e98731ffaafc98bb41cc49b4ba
JFays <joel.fays@jfays.net>
2016-08-24 20:52:10 +0200

    Suppression commentaire

--- Models/CommentService.cs
+++ Models/CommentService.cs
@@ -28,10 +28,9 @@ namespace WebPost.Models
         public void RemoveComment(ObjectId postId, ObjectId commentId)
         {
             var filter = Builders<Post>.Filter.Eq("_id", postId);
-            var filterComment = Builders<Comment>.Filter.Eq("_id", commentId);
-            var update = Builders<Post>.Update.Pull("Comments", filterComment).Inc("TotalComments", -1);
+            var update = Builders<Post>.Update.PullFilter("Comments", Builders<Comment>.Filter.Eq("_id", commentId)).Inc("TotalComments", -1);
 
-            _posts.Collection.UpdateOne(filter, update);
+            _posts.Collection.FindOneAndUpdate(filter, update);
         }
 
         public IList<Comment> GetComments(ObjectId postId, int skip, int limit, int totalComments)
@@ -40,8 +39,10 @@ namespace WebPost.Models
             skip += newComments;
 
             var filter = Builders<Post>.Filter.Eq("_id", postId);
-            var projection = Builders<Post>.Projection.Exclude("Date").Exclude("Title").Exclude("Url").Exclude("Summary")
-                    .Exclude("Details").Exclude("Author").Exclude("TotalComments").Slice("Comments", -skip, limit);
+            var projection = Builders<Post>.Projection
+                    .Exclude("Date").Exclude("Title").Exclude("Url").Exclude("Summary").Exclude("Details").Exclude("Author").Exclude("TotalComments")
+                    .Slice("Comments", -skip, limit);
+
             var post = _posts.Collection.Find(filter).Project<Post>(projection).Single();
 
             return post.Comments.OrderByDescending(c => c.Date).ToList();
@@ -51,6 +52,7 @@ namespace WebPost.Models
         {
             var filter = Builders<Post>.Filter.Eq("_id", postId);
             var projection = Builders<Post>.Projection.Include("TotalComments");
+
             var post = _posts.Collection.Find(filter).Project<Post>(projection).Single();
 
             return post.TotalComments;
